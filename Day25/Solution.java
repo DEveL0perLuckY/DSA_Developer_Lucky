ab62e720f9a6e1e96c97b2b5f9ff3f19be1fddfa
lucky <luckymourya52132@gmail.com>
2024-05-02 21:00:04 +0530

    Day25

--- Day25/Solution.java
+++ Day25/Solution.java
@@ -1,16 +1,17 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
+
         // HashMap<Integer,Integer> map=new HashMap<>();
         // for(int x :nums){
-        //     map.put(x,map.getOrDefault(x,0)+1);
-        //     if(map.get(x) <k &&map.get(x)>0){
-        //         return true;
-        //     }
+        // map.put(x,map.getOrDefault(x,0)+1);
+        // if(map.get(x) <k &&map.get(x)>0){
+        // return true;
+        // }
         // }
         // return false;
-        
-        HashMap<Integer,Integer> map = new HashMap<>();
-       
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
         for (int i = 0; i < nums.length; i++) {
             int num = nums[i];
             if (map.containsKey(num) && i - map.get(num) <= k) {
@@ -19,6 +20,6 @@ class Solution {
             map.put(num, i);
         }
         return false;
-  
+
     }
 }
